6531dad57a7854b4e99dab206e9e6c1c1ba2a20b
Hatem Hosny <hatemhosny@gmail.com>
2016-12-30 02:00:48 +0200

    format article list

--- src/app/articles/article.service.ts
+++ src/app/articles/article.service.ts
@@ -14,33 +14,37 @@ export class ArticleService {
 
   private url: string = environment.papersApiUrl + environment.apiArticles;
 
-  private parameters: string = '_query=[*].{' +
-                                  'id:id,' +
-                                  'title:title.rendered,' +
-                                  'journal:acf.journal,' +
-                                  'types:acf.publication_types,' +
-                                  'duration:acf.audio_duration,' +
-                                  'date:acf.publication_date,' +
-                                  'year:acf.year,' +
-                                  'month:acf.month,' +
-                                  'abstract:acf.abstract}';
+  private filters: string = 'filter[meta_key]=journal&filter[meta_value]=circulation' ;
+  private fields = {
+                    'id': 'id',
+                    'title': 'title.rendered',
+                    'journal': 'acf.journal',
+                    'types': 'acf.publication_types',
+                    'duration': 'acf.audio_duration',
+                    'date': 'acf.publication_date',
+                    'year': 'acf.year',
+                    'month': 'acf.month',
+                    'abstract': 'acf.abstract'
+                  };
 
-  private client: string = '?client=' + environment.client;
+  private parameters: string = '_query=[*].' + JSON.stringify(this.fields).replace(/"/g, '');
+  private client: string = 'client=' + environment.client;
 
-  private fullUrl: string = this.url + '?' + this.parameters + '&' + this.client;
+  private fullUrl: string = this.url + '?' + this.parameters + '&' + this.filters + '&' + this.client;
+
 
   constructor(private http: Http) { }
 
   getArticles(): Observable<IArticle[]> {
     return this.http.get(this.fullUrl)
       .map((response: Response) => <IArticle[]>response.json())
-      // .do(data => console.log('All: ' +  JSON.stringify(data)))
+       .do(data => console.log(this.fullUrl))
       .catch(this.handleError);
   }
 
   getArticle(id: number): Observable<IArticle> {
     return this.getArticles()
-      .map((articles: IArticle[]) => articles.find(a => a.id === id));
+      .map((articles: IArticle[]) => articles.find(article => article.id === id));
   }
 
 
