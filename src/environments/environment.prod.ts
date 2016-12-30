a8ceec9e74a59db1fcd1359de8c325b823e70407
Hatem Hosny <hatemhosny@gmail.com>
2016-12-30 08:20:58 +0200

    add articles API variables to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  envName: 'production'
-
+  envName: 'production',
+  papersApiUrl: 'http://dev.papers.fm/',
+  apiArticles: 'wp-json/wp/v2/articles/',
+  apiJournals: 'wp-json/wp/v2/journals/',
+  client: 'web'
 };
--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  envName: 'staging'
-
+  envName: 'staging',
+  papersApiUrl: 'http://dev.papers.fm/',
+  apiArticles: 'wp-json/wp/v2/articles/',
+  apiJournals: 'wp-json/wp/v2/journals/',
+  client: 'web'
 };
