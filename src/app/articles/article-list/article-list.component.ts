e14d7e443a450cc466da0f2f5bad919a4889c9ce
Hatem Hosny <hatemhosny@gmail.com>
2017-01-05 21:47:14 +0200

    moved all paging logic to PaginationComponent

--- src/app/articles/article-list/article-list.component.ts
+++ src/app/articles/article-list/article-list.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 import { ArticleService } from './../article.service';
 import { IArticle } from '../article.model';
@@ -9,77 +7,49 @@ import { IArticle } from '../article.model';
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.scss']
 })
-export class ArticleListComponent implements OnInit, OnDestroy {
+export class ArticleListComponent implements OnInit, AfterViewInit {
 
-  items: IArticle[];
-  errorMessage: string;
-  currentPage: number = 1;
-  itemsPerPage: number;
-  totalItems: number;
-  loading: boolean = false;
-  subscription: Subscription;
+  private items: IArticle[];
+  private errorMessage: string;
+  private loading: boolean = false;
+
+  private currentPage: number = 1;
+  private itemsPerPage: number;
+  private totalItems: number;
+  private pageQuery: number;
 
   constructor(
-    private articleService: ArticleService,
-    private activatedRoute: ActivatedRoute,
-    private router: Router
+    private articleService: ArticleService
     ) { }
 
   ngOnInit(): void {
     this.itemsPerPage = this.articleService.getNumberOfArticlesPerPage();
-    this.loadArticles(this.getPageQuery());
   }
 
-  ngOnDestroy() {
-    // prevent memory leak by unsubscribing
-    this.subscription.unsubscribe();
+  ngAfterViewInit(): void {
+    this.loadArticles(this.pageQuery);
   }
 
   loadArticles(page: number) {
 
     this.loading = true;
 
-    this.articleService.getArticles(page)
-                .subscribe(
-                  articles => {
-                    this.items = articles;
-                    this.totalItems = this.articleService.getNumberOfTotalItems();
+    this.articleService.getArticles(page).subscribe(
+      articles => {
+        this.items = articles;
+        this.totalItems = this.articleService.getNumberOfTotalItems();
 
-                    this.currentPage = page;
-                    this.loading = false;
-                    this.errorMessage = null;
-                    this.updatePageQuery(page);
-                    window.scrollTo(0, 0);  // TODO smooth scroll
-                  },
-                  error => {
-                    this.loading = false;
-                    this.errorMessage = <any>error;  // TODO log error
-                  });
-  }
-
-  // get QueryString('page') to allow deep linking
-  private getPageQuery(): number {
-    let page = this.currentPage;
-    // subscribe to router event
-    this.subscription = this.activatedRoute.queryParams.subscribe(
-      (param: any) => {
-        if (!isNaN(param['page']) && Number(param['page']) > 0) {
-          page = Number(param['page']);
-        }
-      });
-    return page;
-  }
-
-  // update QueryString('page') to match currentPage
-  private updatePageQuery(page: number) {
-    // get route without parameters
-    let route = this.router.url.split('?')[0];  // TODO improve this
-
-    if (page === 1) {
-      this.router.navigate([route]);
-    } else {
-      this.router.navigate([route], {queryParams: {'page': page}});  // TODO allow other filters and sorting
-    }
+        this.currentPage = page;
+        this.loading = false;
+        this.errorMessage = null;
+        this.pageQuery = page;
+        window.scrollTo(0, 0);  // TODO smooth scroll
+      },
+      error => {
+        this.loading = false;
+        this.errorMessage = <any>error;  // TODO log error
+      }
+    );
   }
 
 }
